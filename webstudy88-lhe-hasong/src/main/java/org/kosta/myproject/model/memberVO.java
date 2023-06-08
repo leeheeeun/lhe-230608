5cc79dc07f6003af4ef2cbb9f5d8e6992e893d53
shj22k <shj22k@naver.com>
2023-06-08 17:44:08 +0900

    후후

--- webstudy88-lhe-hasong/src/main/java/org/kosta/myproject/model/memberVO.java
+++ webstudy88-lhe-hasong/src/main/java/org/kosta/myproject/model/memberVO.java
@@ -1,12 +1,10 @@
 package org.kosta.myproject.model;
 
 public class memberVO {
-<<<<<<< HEAD
 	private String id;
 	private String member;
-=======
 	private String changeId;
->>>>>>> branch 'main' of https://github.com/leeheeeun/lhe-230608.git
+
 
 	public String getChangeId() {
 		return changeId;
